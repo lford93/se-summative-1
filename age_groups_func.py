42788efbc55e9667814235165fb21e3b19887f9e
Lewis <lf20204@students.nulondon.ac.uk>
2023-01-13 14:16:50 +0000

    added initial age_groups_func.py

---
+++ age_groups_func.py
@@ -0,0 +1,28 @@
+# import numpy to be able to use np.array
+import numpy as np
+
+# create the function in this chunk
+def age_groups(ages, breaks):
+    """
+    Create age bands from age
+
+    This function creates age bands based on the input of ages and the breaks. 
+    The output is a list of corresponding age bands for the inputs.
+
+    Parameters
+    ----------
+    ages : int
+        A list of integers or a single integer used as age to be converted to a corresponding age band.
+    breaks : int
+        A list of integers to be used to create the breaks for the age bands.
+
+    Returns
+    -------
+    str
+        The corresponding age bands of the inputted ages.
+
+    Examples
+    --------
+    >>> 
+    """
+    return ages
